78ae70284a9396d13a969ecbdaed21617b3fa089
Sava-Lakicevic <105124288+Sava-Lakicevic@users.noreply.github.com>
2022-09-27 11:14:26 +0200

    Update search.py

--- search.py
+++ search.py
@@ -5,50 +5,38 @@ import glob
 import sys
 import re
 
-
+# flatten the list for all excel files
 def flatten_list(l):
     return [item for sublist in l for item in sublist]
 
+#uses glob to join path with all files ending with the .xlsx extention
 def get_all_excel_files():
+#     get current working directory
     path = os.getcwd()
     excel_files = []
     for folder, _, _ in os.walk(path):
         excel_files.append((glob.glob(os.path.join(folder, "*.xlsx"))))
     return flatten_list(excel_files)
 
-# def main():
-#     excel_files = get_all_excel_files()
-#     input_string = ''
-#     for arg in sys.argv[1:]:
-#         input_string += arg + ' '
-#     input_string = input_string.strip().lower()
-#     for f in excel_files:
-#         df = pd.read_excel(f, header=None, sheet_name=None)
-#         file_name = f.split("\\")[-2:]
-#         for sheet_name, sheet_data in df.items():
-#             sheet_data = sheet_data.fillna('-')
-#             values = sheet_data.values
-#             for row in range(len(values)):
-#                 for col in range(len(values[0])):
-#                     check_string = str(values[row][col]).lower()
-#                     if input_string in check_string:
-#                         print(f'File: {file_name}; Sheet: {sheet_name}; Location: ({row}, {col})')
-
 def main():
     excel_files = get_all_excel_files()
     while True:
         input_string = input('What are you looking for: ').strip().lower()
-        if input_string == 'kraj rada':
+        if input_string == 'end of work':
             break
         for f in excel_files:
+#             take the last 2 inputs of the file path, which inlcudes the containing folder and the file name
             file_name = f.split("\\")[-2:]
             try:
                 df = pd.read_excel(f, header=None, sheet_name=None)
             except PermissionError:
+#                 certain temporary or corrupted files cannot be accessed
                 print(f'NO PERMISSION TO OPEN {file_name}')
             except ValueError:
+#                 certain temporary or corrupted files can be accessed, but throw value error
                 print(f'FAILED TO OPEN {file_name}')
             except:
+#                 handle unexpected errors
                 print(f'UNKOWN ERROR FOR {file_name}')
             for sheet_name, sheet_data in df.items():
                 sheet_data = sheet_data.fillna('-')
@@ -56,6 +44,7 @@ def main():
                 for row in range(len(values)):
                     for col in range(len(values[0])):
                         check_string = str(values[row][col]).lower()
+#                         check if the search string is in each cell of the excel file, in order to track the row and column
                         if input_string in check_string:
                             print(f'File: {file_name}; Sheet: {sheet_name}; Location: ({row}, {col})')
 
