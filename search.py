a751939c667248ae0b9130fbd0dda80c239ca48b
Sava-Lakicevic <105124288+Sava-Lakicevic@users.noreply.github.com>
2022-09-30 09:26:48 +0200

    Update search.py
    Moved the for loop for searching through files within the try code block

--- search.py
+++ search.py
@@ -11,7 +11,7 @@ def flatten_list(l):
 
 #uses glob to join path with all files ending with the .xlsx extention
 def get_all_excel_files():
-#     get current working directory
+    # get current working directory
     path = os.getcwd()
     excel_files = []
     for folder, _, _ in os.walk(path):
@@ -25,28 +25,31 @@ def main():
         if input_string == 'end of work':
             break
         for f in excel_files:
-#             take the last 2 inputs of the file path, which inlcudes the containing folder and the file name
+            # take the last 2 inputs of the file path, which inlcudes the containing folder and the file name
             file_name = f.split("\\")[-2:]
             try:
                 df = pd.read_excel(f, header=None, sheet_name=None)
-            except PermissionError:
-#                 certain temporary or corrupted files cannot be accessed
-                print(f'NO PERMISSION TO OPEN {file_name}')
-            except ValueError:
-#                 certain temporary or corrupted files can be accessed, but throw value error
-                print(f'FAILED TO OPEN {file_name}')
-            except:
-#                 handle unexpected errors
-                print(f'UNKOWN ERROR FOR {file_name}')
-            for sheet_name, sheet_data in df.items():
+                for sheet_name, sheet_data in df.items():
+                # if you delete NaN values, you delete the entire row (or column, depending on args)
+                # if you don't feel the NaN values, you can't compare strings
                 sheet_data = sheet_data.fillna('-')
                 values = sheet_data.values
                 for row in range(len(values)):
                     for col in range(len(values[0])):
                         check_string = str(values[row][col]).lower()
-#                         check if the search string is in each cell of the excel file, in order to track the row and column
+                        # check if the search string is in each cell of the excel file, in order to track the row and column
                         if input_string in check_string:
                             print(f'File: {file_name}; Sheet: {sheet_name}; Location: ({row}, {col})')
+            except PermissionError:
+                # certain temporary or corrupted files cannot be accessed
+                print(f'NO PERMISSION TO OPEN {file_name}')
+            except ValueError:
+                # certain temporary or corrupted files can be accessed, but throw value error
+                print(f'FAILED TO OPEN {file_name}')
+            except:
+                # handle unexpected errors
+                print(f'UNKOWN ERROR FOR {file_name}')
+            
 
 
 if __name__ == "__main__":
